d3574cf76c228a87ac02018c2fb2b78c4be38e0c
Matías <matiasarce214@gmail.com>
2024-08-16 22:11:40 -0300

    Configuración de navbar

--- Appweb/urls.py
+++ Appweb/urls.py
@@ -3,4 +3,20 @@ from .views import *
 
 urlpatterns = [
     path('', ventana_inicio, name="inicio"),
+    path('vinilosLista/', ViniloListView.as_view(), name='ListarVinilos'),
+    path('vinilosDetalle/<pk>', ViniloDetailView.as_view(), name='VinilosDetalle'),
+    path('vinilosCrear/', ViniloCreateView.as_view(), name='CrearVinilo'),
+    path('vinilosEditar/<pk>', ViniloUpdateView.as_view(), name='EditarVinilo'),
+    path('vinilosBorrar/<pk>', ViniloDeleteView.as_view(), name='BorrarVinilo'),
+    path('tocadiscosLista/', TocadiscosListView.as_view(), name='ListarTocadiscos'),
+    path('tocadiscosDetalle/<pk>', TocadiscosDetailView.as_view(), name='DetalleTocadiscos'),
+    path('tocadiscosCrear/', TocadiscosCreateView.as_view(), name='CrearTocadiscos'),
+    path('tocadiscosEditar/<pk>', TocadiscosUpdateView.as_view(), name='EditarTocadiscos'),
+    path('tocadiscosBorrar/<pk>', TocadiscosDeleteView.as_view(), name='BorrarTocadiscos'),
+    path('parlantesLista/', ParlanteListView.as_view(), name='ListarParlantes'),
+    path('parlantesDetalle/<pk>', ParlanteDetailView.as_view(), name='DetalleParlantes'),
+    path('parlantesCrear/', ParlanteCreateView.as_view(), name='CrearParlantes'),
+    path('parlantesEditar/<pk>', ParlanteUpdateView.as_view(), name='EditarParlantes'),
+    path('parlantesBorrar/<pk>', ParlanteDeleteView.as_view(), name='BorrarParlantes'),
+
 ]
--- Appweb/views.py
+++ Appweb/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import *
 
 def ventana_inicio(request):
@@ -9,3 +12,76 @@ class ViniloListView(ListView):
     model = Vinilo
     context_object_name = 'vinilo'
     template_name = 'ventanas/vinilos_lista.html'
+
+class ViniloDetailView(DetailView):
+    model = Vinilo
+    template_name = 'ventanas/vinilos_detalle.html'
+
+class ViniloCreateView(CreateView):
+    model = Vinilo
+    template_name = 'ventanas/vinilos_crear.html'
+    success_url = reverse_lazy('ListarVinilos')
+    fields = ['artista', 'album', 'genero', 'precio']
+
+class ViniloUpdateView(UpdateView):
+    model = Vinilo
+    template_name = 'ventanas/vinilos_editar.html'
+    success_url = reverse_lazy('ListarVinilos')
+    fields = ['artista', 'album', 'genero', 'precio']
+
+class ViniloDeleteView(DeleteView):
+    model = Vinilo
+    template_name = 'ventanas/vinilos_borrar.html'
+    success_url = reverse_lazy('ListarVinilos')
+
+class TocadiscosListView(ListView):
+    model = Reproductor
+    context_object_name = 'reproductor'
+    template_name = 'ventanas/tocadiscos_lista.html'
+
+class TocadiscosDetailView(DetailView):
+    model = Reproductor
+    template_name = 'ventanas/tocadiscos_detalle.html'
+
+class TocadiscosCreateView(CreateView):
+    model = Reproductor
+    template_name = 'ventanas/tocadiscos_crear.html'
+    success_url = reverse_lazy('ListarTocadiscos')
+    fields = ['marca', 'modelo', 'precio']
+
+class TocadiscosUpdateView(UpdateView):
+    model = Reproductor
+    template_name = 'ventanas/tocadiscos_editar.html'
+    success_url = reverse_lazy('ListarTocadiscos')
+    fields = ['marca', 'modelo', 'precio']
+
+class TocadiscosDeleteView(DeleteView):
+    model = Reproductor
+    template_name = 'ventanas/tocadiscos_borrar.html'
+    success_url = reverse_lazy('ListarTocadiscos')
+
+class ParlanteListView(ListView):
+    model = Parlante
+    context_object_name = 'parlante'
+    template_name = 'ventanas/parlantes_lista.html'
+
+class ParlanteDetailView(DetailView):
+    model = Parlante
+    template_name = ''
+
+class ParlanteCreateView(CreateView):
+    model = Parlante
+    template_name = ''
+    success_url = reverse_lazy('ListarParlantes')
+    fields = ['marca', 'tipo', 'potencia', 'precio']
+
+class ParlanteUpdateView(UpdateView):
+    model = Parlante
+    template_name = ''
+    success_url = reverse_lazy('ListarParlantes')
+    fields = ['marca', 'tipo', 'potencia', 'precio']
+
+class ParlanteDeleteView(DeleteView):
+    model = Parlante
+    template_name = ''
+    success_url = reverse_lazy('ListarParlantes')
